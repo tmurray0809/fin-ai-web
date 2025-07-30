25d09f47912b187cd43b7b1e90c8849dc0b32fd6
tmurray0809 <murra340@purdue.edu>
2025-07-29 19:13:07 -0500

    front and backend working together

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from classes.user import User
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def root():
+    return {"message":"Hello, world!"}
+
+@app.post("/login")
+async def login(data: User):
+    print(data)
+    if data.email == "tmurray2024@gmail.com" and data.password == "Parkview2015!":
+        return {"message":"success"}
+    raise HTTPException(status_code=401, detail="Incorrect Username or Password")
--- frontend/test.js
+++ frontend/test.js
@@ -1,12 +1,25 @@
 const form = document.getElementById('loginForm');
 const output = document.getElementById('output');
 
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', async function (event) {
     event.preventDefault();
 
     const email = document.getElementById('email1').value;
     const password = document.getElementById('password1').value;
 
-    output.textContent = `Email: ${email}, Password: ${password}`;
+    const response = await fetch("http://127.0.0.1:8000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({email, password})
+    });
 
+    if (response.ok) {
+        const data = await response.json();
+        output.textContent = `Success`;
+    } else {
+        const data = await response.text();
+        output.textContent = `Error`;
+    }
 });
